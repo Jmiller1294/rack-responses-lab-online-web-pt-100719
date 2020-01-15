9535dea2e7accd574f735f20b7270bbced723853
Justin Miller <jayman1294@gmail.com>
2020-01-15 01:45:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,3 +2,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
+    if Time.now >= 12
+      resp.write "Good Afternoon!"
+    else 
+      resp.write "Good Morning!"
+    end
+    
+  resp.finish
